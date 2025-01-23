ecabf41889f1c29c0603bf79cdb91c6877374d77
zakhar <zaharsavchen@gmail.com>
2025-01-23 20:26:42 +0200

    rewrite full_name property in Actor model

--- cinema/models.py
+++ cinema/models.py
@@ -29,7 +29,7 @@ class Actor(models.Model):
 
     @property
     def full_name(self) -> str:
-        return str(self)
+        return f"{self.first_name} {self.last_name}"
 
     def __str__(self):
         return self.first_name + " " + self.last_name
